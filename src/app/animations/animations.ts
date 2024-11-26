e44069b949b53a5d3f864ddff0c3625e173a7bcb
jonybhm <jony.bhm@gmail.com>
2024-11-26 10:38:44 -0300

    added animation

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -1,35 +1,35 @@
 import { trigger, group, style, animate, transition, query, animateChild, state } from '@angular/animations';
 
 
-// export const slideInAnimation = trigger('routeAnimations', [
-//   transition('* <=> *', [
-//     style({ position: 'relative' }),
-//     query(':enter, :leave', [
-//       style({
-//         position: 'relative',
-//         top: 0,
-//         left: 0,
-//         width: '100%',
-//         opacity: 0,
-//         filter: 'blur(5px)'
-//       })
-//     ], { optional: true }),
-//     query(':enter', [
-//       style({ opacity: 0, filter: 'blur(10px)' })
-//     ], { optional: true }),
-//     group([
-//       query(':leave', [
-//         animate('1000ms ease-out', style({ opacity: 0, filter: 'blur(10px)' }))
-//       ], { optional: true }),
-//       query(':enter', [
-//         animate('1000ms ease-out', style({ opacity: 1, filter: 'blur(0px)' }))
-//       ], { optional: true })
-//     ])
-//   ])
-// ]);
 
 
 export const slideInAnimation = trigger('routeAnimations', [
+
+  transition('home <=> estadisticas', [
+    style({ position: 'relative' }),
+    query(':enter, :leave', [
+      style({
+        position: 'relative',
+        top: 0,
+        left: 0,
+        width: '100%',
+        opacity: 0,
+        filter: 'blur(5px)'
+      })
+    ], { optional: true }),
+    query(':enter', [
+      style({ opacity: 0, filter: 'blur(10px)' })
+    ], { optional: true }),
+    group([
+      query(':leave', [
+        animate('1000ms ease-out', style({ opacity: 0, filter: 'blur(10px)' }))
+      ], { optional: true }),
+      query(':enter', [
+        animate('1000ms ease-out', style({ opacity: 1, filter: 'blur(0px)' }))
+      ], { optional: true })
+    ])
+  ]),
+
   transition('* <=> *', [
     style({ opacity: 0, transform: 'translateY(-200%)' }),
     animate('700ms cubic-bezier(0.68, -0.55, 0.27, 1.55)', style({ opacity: 1, transform: 'translateY(0)' }))
